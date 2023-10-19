bc9ffcaf2959a7ffcbcf365e498bcae34fd9ef80
jackeylv <lvlin@whu.edu.cn>
2023-10-19 17:44:04 +0800

    lab支持多文件夹批量统计

--- cmd/lab.go
+++ cmd/lab.go
@@ -23,6 +23,7 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -35,7 +36,7 @@ import (
 
 var (
 	workingDir string
-	labName    string
+	labsName   []string
 	// like php-2023-class-1
 	coursename string
 	debug      bool
@@ -57,8 +58,8 @@ The namelist is a csv type file with 'name' and 'no' columns.
 The reports in the given directory are in the format of '$name-$no-$lab.doc' or '$name-$no-$lab.docx'.
 The generated result includes the submmited flag for each student and those file with illegal filename format.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if labName == "" {
-			panic(fmt.Errorf("labName is empty"))
+		if len(labsName) == 0 {
+			labsName = listSubDirectories(workingDir)
 		}
 		var excelFile string
 		if coursename == "" {
@@ -67,12 +68,12 @@ The generated result includes the submmited flag for each student and those file
 			excelFile = viper.GetString("lab.class." + coursename)
 		}
 		if debug {
-			fmt.Fprintln(os.Stderr, "workingDir:", workingDir, "labName:", labName, "csvfile:", excelFile)
+			fmt.Fprintln(os.Stderr, "workingDir:", workingDir, "labName:", labsName, "csvfile:", excelFile)
 		}
 		students := readNameList(excelFile)
 		// 文件名模式: `.*\.(doc|docx)` 表示匹配所有以 .doc 或 .docx 结尾的文件
 		fileNamePattern := `.*\.(doc|docx)`
-		traverseFiles(workingDir, labName, students, fileNamePattern)
+		traverseFiles(workingDir, labsName, students, fileNamePattern)
 	},
 }
 
@@ -92,8 +93,23 @@ func init() {
 		"the directory contains reports.")
 	labCmd.Flags().StringVarP(&coursename, "coursename", "c", "",
 		"The coursename , like php-2023-class-1")
-	labCmd.Flags().StringVarP(&labName, "labName", "l", "", "the lab name in filename")
+	labCmd.Flags().StringSliceVarP(&labsName, "labName", "l", []string{}, "the labs' names in filename, split with comma.")
 	labCmd.Flags().BoolVarP(&debug, "debug", "D", false, "show debug result or only the result")
+}
+
+// List all the sub-directories in the given path
+func listSubDirectories(path string) []string {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+	var subDirs []string
+	for _, f := range files {
+		if f.IsDir() {
+			subDirs = append(subDirs, f.Name())
+		}
+	}
+	return subDirs
 }
 
 func readNameList(excelFile string) []CourseStudent {
@@ -111,79 +127,139 @@ func readNameList(excelFile string) []CourseStudent {
 	return lines
 }
 
-func traverseFiles(folderPath, labName string, students []CourseStudent, fileNamePattern string) {
-	illegalFileNames := make([]string, 0)
-	notFounds := make([]string, 0)
-	result := make([]string, len(students))
-	found := 0
-	for i := 1; i < len(result); i++ {
-		// Not submitted at default
-		result[i] = ""
-	}
-	err := filepath.Walk(folderPath, func(path string, info os.FileInfo, err error) error {
+func traverseFiles(folderPath string, labsName []string, students []CourseStudent, fileNamePattern string) {
+	// Not submitted at default
+	illegalFileNames, notFounds, result, found := initResultSet(labsName, students)
+	for j, labName := range labsName {
+		root := filepath.Join(folderPath, labName)
+		// 如果不存在，将该文件名添加到未匹配数组中
+		// 存在，标记为已提交
+		err := processOneLab(root, fileNamePattern, illegalFileNames, j, labName, students, notFounds, result, found)
+
 		if err != nil {
 			fmt.Println("Error:", err)
-			return nil
+		}
+	}
+
+	handleResult(found, labsName, result, students, illegalFileNames, notFounds)
+}
+
+func processOneLab(labDir string,
+	fileNamePattern string,
+	illegalFileNames [][]string,
+	labIndex int,
+	labName string,
+	students []CourseStudent,
+	notFounds [][]string,
+	result [][]string,
+	found []int) error {
+	err := filepath.Walk(labDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			panic(fmt.Errorf("prevent panic by handling failure accessing a path %q: %v", path, err))
 		}
 
 		fileName := filepath.Base(path)
 		if match, _ := regexp.MatchString(fileNamePattern, fileName); match {
 			fields := strings.Split(fileName, "-")
 			if len(fields) < 3 {
-				illegalFileNames = append(illegalFileNames, fileName)
+				illegalFileNames[labIndex] = append(illegalFileNames[labIndex], fileName)
 				return nil
 			}
 			name, sno, experiment := fields[0], fields[1], strings.Join(fields[2:], "-")
 			experiment = strings.Split(experiment, ".")[0]
 			if experiment != labName {
-				illegalFileNames = append(illegalFileNames, fileName)
+				illegalFileNames[labIndex] = append(illegalFileNames[labIndex], fileName)
 				return nil
 			}
 			idx := findRecord(students, name, sno)
 			if idx == -1 {
-				// 如果不存在，将该文件名添加到未匹配数组中
-				notFounds = append(notFounds, fileName)
+
+				notFounds[labIndex] = append(notFounds[labIndex], fileName)
 			} else {
-				// 存在，标记为已提交
-				result[idx] = "已提交"
-				found++
+
+				if result[idx][labIndex] != "已提交" {
+					result[idx][labIndex] = "已提交"
+					found[labIndex]++
+				} else {
+					fmt.Fprintf(os.Stderr, "Duplicate file name: %s\n", fileName)
+				}
 			}
 		}
 
 		return nil
 	})
+	return err
+}
 
-	if err != nil {
-		fmt.Println("Error:", err)
+func handleResult(found []int,
+	labsName []string,
+	result [][]string,
+	students []CourseStudent,
+	illegalFileNames [][]string,
+	notFounds [][]string) {
+	fmt.Println("Found:")
+	for i, v := range found {
+		fmt.Printf("%d", v)
+		if i < len(found)-1 {
+			fmt.Print(",")
+		}
 	}
-	fmt.Printf("Found %d\n", found)
+	fmt.Println()
 	if debug {
-		fmt.Printf("%s,%s,%s\n", "Name", "Sno", labName)
+		fmt.Printf("%s,%s,%s\n", "Name", "Sno", strings.Join(labsName, ","))
 	} else {
-		fmt.Println(labName)
+		fmt.Println(strings.Join(labsName, ","))
 	}
 	for i := 0; i < len(result); i++ {
 		if debug {
-			fmt.Printf("%s,%s,%s\n", students[i].Name, students[i].Sno, result[i])
+			fmt.Printf("%s,%s,%s\n", students[i].Name, students[i].Sno, strings.Join(result[i], ","))
 		} else {
-			fmt.Println(result[i])
+			fmt.Println(strings.Join(result[i], ","))
 		}
 	}
 	fmt.Println("---------")
+	// print files does not match the filepattern
 	if len(illegalFileNames) > 0 {
 		fmt.Fprintln(os.Stderr, "Illegal file name:")
-		for _, v := range illegalFileNames {
-			fmt.Fprintln(os.Stderr, v)
+		for i, v := range illegalFileNames {
+			if len(v) > 0 {
+				fmt.Fprintln(os.Stderr, labsName[i])
+				for _, v2 := range v {
+					fmt.Fprintln(os.Stderr, v2)
+				}
+			}
 		}
-		fmt.Println("---------")
+		fmt.Fprintln(os.Stderr, "---------")
 	}
+	// print files with name or no missmatched.
 	if len(notFounds) > 0 {
 		fmt.Fprintln(os.Stderr, "Not found:")
-		for _, v := range notFounds {
-			fmt.Fprintln(os.Stderr, v)
+		for i, v := range notFounds {
+			if len(v) > 0 {
+				fmt.Fprintln(os.Stderr, labsName[i])
+				for _, v2 := range v {
+					fmt.Fprintln(os.Stderr, v2)
+				}
+			}
 		}
-		fmt.Println("---------")
+		fmt.Fprintln(os.Stderr, "---------")
 	}
+}
+
+func initResultSet(labsName []string, students []CourseStudent) ([][]string, [][]string, [][]string, []int) {
+	illegalFileNames := make([][]string, len(labsName))
+	notFounds := make([][]string, len(labsName))
+	result := make([][]string, len(students))
+	found := make([]int, len(labsName))
+
+	for i := 0; i < len(students); i++ {
+		result[i] = make([]string, len(labsName))
+		for j := 0; j < len(labsName); j++ {
+
+			result[i][j] = ""
+		}
+	}
+	return illegalFileNames, notFounds, result, found
 }
 
 // Return the index of first found record, else return -1
